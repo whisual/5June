2ea1a807f504ba669085788788241b8d409607cc
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2022-06-05 13:31:06 +0530

    Add files via upload

---
+++ ques2.js
@@ -0,0 +1,6 @@
+var n = parseInt(prompt("Enter Number:"));
+var cube;
+
+var cube = n * n * n;
+
+console.log(cube);
---
+++ ques3.js
@@ -0,0 +1,6 @@
+let i = parseInt(prompt("Enter Number:"));
+sum = 0;
+for (i = 1; i <= 20; i += 2) {
+    sum = sum + i;
+}
+console.log(sum);
